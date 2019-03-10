49fd9cfd8a8adbfca1880f4b66dca722588c8997
Chance Hudson <jchancehud@gmail.com>
2019-03-09 21:03:15 -0600

    Add comments

--- connectMiddleware.js
+++ connectMiddleware.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+/**
+ * Establish a connection to the mongo database, then continue the request
+ **/
 module.exports = (req, res, next) => {
   mongoose.connect(process.env.DB_URI, {
     connectTimeoutMS: 5000,
