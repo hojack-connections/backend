24f0dca81d51f71f8e2588be9930c39d11b0d465
Chance Hudson <jchancehud@gmail.com>
2019-03-09 21:02:14 -0600

    Fix login, signup methods, establish mongo connection on per request basis

---
+++ routes/login.js
@@ -0,0 +1,43 @@
+const User = require('../models/user');
+const connectMiddleware = require('../connectMiddleware');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const asyncHandler = require('express-async-handler');
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(connectMiddleware);
+
+app.post('*', asyncHandler(login));
+
+/**
+ * Authenticate and generate a jsonwebtoken
+ **/
+async function login(req, res) {
+  const users = await User.find({
+    email: req.body.email
+  }).exec();
+  if (users.length === 0) {
+    res.status(400);
+    res.send('Email not found.');
+    return;
+  }
+  const isPasswordMatch = await bcrypt.compare(req.body.password, users[0].passwordHash);
+  if (!isPasswordMatch) {
+    res.status(401);
+    res.send('There was a problem authenticating.');
+    return;
+  }
+  const token = jwt.sign({
+    firstname: users[0].firstname,
+    lastname: users[0].lastname,
+    email: users[0].email,
+    _id: users[0]._id
+  }, process.env.WEB_TOKEN_SECRET);
+  res.json({ token });
+}
+
+module.exports = app;
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
-const User = require('../models/user');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
+const User = require('../models/user');
+const connectMiddleware = require('../connectMiddleware');
+const bcrypt = require('bcrypt');
 const emailValidator = require('email-validator');
 const asyncHandler = require('express-async-handler');
 const bodyParser = require('body-parser');
@@ -9,8 +9,9 @@ const express = require('express');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(connectMiddleware);
 
-app.get('*', asyncHandler(login));
+app.get('*', asyncHandler(getUser));
 app.post('*', asyncHandler(signup));
 
 /**
@@ -22,12 +23,12 @@ async function signup(req, res) {
     res.send('Invalid email supplied.');
     return;
   }
-  const emailMatches = await User.find({
+  const emailCount = await User.countDocuments({
     email: {
       $regex: new RegExp(`^${req.body.email}$`, 'i')
     }
-  });
-  if (emailMatches && emailMatches.length > 0) {
+  }).exec();
+  if (emailCount > 0) {
     res.status(400);
     res.send('This email is already registered. Please try another or login.');
     return;
@@ -39,41 +40,28 @@ async function signup(req, res) {
   }
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(req.body.password, salt);
-  const newUser = new User({
+  const created = await User.create({
     firstname: req.body.firstname,
     lastname: req.body.lastname,
     passwordHash: hash,
     email: req.body.email
   });
-  const created = await newUser.save();
   res.json(created);
 }
 
 /**
- * Authenticate and generate a jsonwebtoken
+ * Retrieve a user by email
  **/
-async function login(req, res) {
+async function getUser(req, res) {
   const users = await User.find({
     email: req.body.email
-  });
+  }).exec();
   if (users.length === 0) {
-    res.status(400);
+    res.status(404);
     res.send('Email not found.');
     return;
   }
-  const isPasswordMatch = await bcrypt.compare(req.body.password, users[0].passwordHash);
-  if (!isPasswordMatch) {
-    res.status(401);
-    res.send('There was a problem authenticating.');
-    return;
-  }
-  const token = jwt.sign({
-    firstname: users[0].firstname,
-    lastname: users[0].lastname,
-    email: users[0].email,
-    _id: users[0]._id
-  }, process.env.WEB_TOKEN_SECRET);
-  res.json({ token });
+  return users[0];
 }
 
 module.exports = app;
