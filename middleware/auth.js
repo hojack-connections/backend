be53c422502832e7d39580a09c56b882de2646b1
Chance Hudson <jchancehud@gmail.com>
2019-03-09 22:51:49 -0600

    Tabs -> spaces

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-	const token = req.body.token || req.query.token;
-	if (!token) {
-		res.status(401);
+  const token = req.body.token || req.query.token;
+  if (!token) {
+    res.status(401);
     res.send('No authentication token supplied in body or query.');
     return;
-	}
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.WEB_TOKEN_SECRET);
