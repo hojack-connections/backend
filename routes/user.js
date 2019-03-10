bfd935d18cc2cd52a3469f286b1911f6ab699271
Chance Hudson <jchancehud@gmail.com>
2019-03-09 20:07:42 -0600

    Add base implementation for login and signup

--- routes/user.js
+++ routes/user.js
@@ -2,21 +2,41 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
+const emailValidator = require('email-validator');
+const asyncHandler = require('express-async-handler');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
-app.get('*', (req, res) => {
-  login(req, res);
-});
+app.use(bodyParser.json());
 
-app.post('*', (req, res) => {
-  signup(req, res).catch(err => console.log('error', err));
-});
+app.get('*', asyncHandler(login));
+app.post('*', asyncHandler(signup));
 
-module.exports = app;
-
+/**
+ * Create a new User record and store in database
+ **/
 async function signup(req, res) {
-  await checkEmailDuplication(req);
+  if (!emailValidator.validate(req.body.email)) {
+    res.status(400);
+    res.send('Invalid email supplied.');
+    return;
+  }
+  const emailMatches = await User.find({
+    email: {
+      $regex: new RegExp(`^${req.body.email}$`, 'i')
+    }
+  });
+  if (emailMatches && emailMatches.length > 0) {
+    res.status(400);
+    res.send('This email is already registered. Please try another or login.');
+    return;
+  }
+  if (!req.body.password || req.body.password.length < 5) {
+    res.status(400);
+    res.send('Please make sure your password is at least 5 character.');
+    return;
+  }
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(req.body.password, salt);
   const newUser = new User({
@@ -29,47 +49,31 @@ async function signup(req, res) {
   res.json(created);
 }
 
-exports.login = function(req, res) {
-    if(!req.body.email || !req.body.password) {
-        return res.status(400).send(config.MISSING_PARAMETER);
-    }
-
-    User.find({email: req.body.email}, function(err, users) {
-        if (err){
-            return res.status(500).send(config.DB_ERROR);
-        }
-        if (users.length == 0){
-            return res.status(401).send(config.AUTHENTICATION_FAILED);
-        }
-
-        bcrypt.compare(req.body.password, users[0].passwordHash, function(err, isPasswordMatch) {
-            if(isPasswordMatch == false) {
-                return res.status(401).send(config.AUTHENTICATION_FAILED);
-            }
-
-            var token = jwt.sign({
-                firstname: users[0].firstname,
-                lastname: users[0].lastname,
-                email: users[0].email,
-                _id: users[0]._id
-            }, config.JWT_SECRET);
-            res.json({
-                token,
-            });
-        });
-
-    });
+/**
+ * Authenticate and generate a jsonwebtoken
+ **/
+async function login(req, res) {
+  const users = await User.find({
+    email: req.body.email
+  });
+  if (users.length === 0) {
+    res.status(400);
+    res.send('Email not found.');
+    return;
+  }
+  const isPasswordMatch = await bcrypt.compare(req.body.password, users[0].passwordHash);
+  if (!isPasswordMatch) {
+    res.status(401);
+    res.send('There was a problem authenticating.');
+    return;
+  }
+  const token = jwt.sign({
+    firstname: users[0].firstname,
+    lastname: users[0].lastname,
+    email: users[0].email,
+    _id: users[0]._id
+  }, process.env.WEB_TOKEN_SECRET);
+  res.json({ token });
 }
 
-function checkEmailDuplication(req) {
-  return User.find({
-    email: {
-      $regex: new RegExp(`^${req.body.email}`, 'i')
-    }
-  })
-    .then(users => {
-      if (users && users.length > 0) {
-        throw new Error('Email address already exists');
-      }
-    });
-}
+module.exports = app;
