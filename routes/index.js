5d51c6a60be8460c5980c1b2a4c160d6fded8c33
Chance Hudson <jchancehud@gmail.com>
2019-03-11 04:28:45 -0500

    Move mongoConnect logic into routes/index

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const mongoConnect = require('../middleware/mongoConnect');
+const mongoose = require('mongoose');
 
 app.use(bodyParser.json());
-app.use(mongoConnect);
+
+/**
+ * Establish a connection to the mongo database, then continue the request
+ **/
+app.use((req, res, next) => {
+  mongoose.connect(process.env.DB_URI, {
+    connectTimeoutMS: 5000,
+    useNewUrlParser: true
+  })
+    .then(() => next())
+    .catch(next);
+});
 
 // Call each other exported function in this directory to allow setup
 require('not-index')(__dirname).map(fn => fn(app));
