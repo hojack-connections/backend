d50e6f5b642aa335c27e2f194aa6fe74469494b5
Chance Hudson <jchancehud@gmail.com>
2019-03-12 02:59:55 -0500

    Update and delete functionality

--- routes/event.js
+++ routes/event.js
@@ -12,6 +12,7 @@ AWS.config = {
 module.exports = app => {
   app.post('/events', auth, asyncHandler(create));
   app.put('/events', auth, asyncHandler(update));
+  app.delete('/events', auth, asyncHandler(_delete));
 };
 
 /**
@@ -20,7 +21,7 @@ module.exports = app => {
 async function create(req, res) {
   const created = await Event.create({
     ...req.body,
-    user: req.user.id
+    user: req.user._id
   });
   res.json(created);
 }
@@ -29,24 +30,63 @@ async function create(req, res) {
  * Update an Event document
  **/
 async function update(req, res) {
-  const _event = await Event.findOne({
+  const doc = await Event.findOne({
     _id: req.body._id
   }).lean().exec();
-  if (!_event) {
+  if (!doc) {
     res.status(404);
     res.send('Unable to find document to update. Please supply an _id property.');
     return;
   }
+  if (doc.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    res.send('You don\'t own this event and cannot update it.');
+    return;
+  }
   const updated = await Event.updateOne({
-    _id: _event._id
+    _id: doc._id
   }, {
     ...req.body
   });
   if (updated.n !== 1) {
-    res.status(400);
-    res.send('No documents modified.');
+    res.status(500);
+    res.send('No documents selected.');
     return;
   }
   res.status(204)
+  res.end();
+}
+
+/**
+ * Delete an Event document
+ **/
+async function _delete(req, res) {
+  if (!req.body._id) {
+    res.status(400);
+    res.send('No _id specified for deletion.');
+    return;
+  }
+  const doc = await Event.findOne({
+    _id: req.body._id
+  }).lean().exec();
+  if (!doc) {
+    res.status(404);
+    res.send('Unable to find document to delete. Please supply an _id property.');
+    return;
+  }
+  if (doc.user.toString() !== req.user._id.toString()) {
+    res.status(401);
+    res.send('You don\'t own this event and cannot delete it.');
+    return;
+  }
+  const deleted = await Event.deleteOne({
+    _id: req.body._id,
+  }).exec();
+  if (deleted.n !== 1) {
+    res.status(500);
+    res.send('No documents deleted.');
+    return;
+  }
+  res.status(204);
   res.end();
 }
